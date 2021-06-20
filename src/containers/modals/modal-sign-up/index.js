e159231c2ca5a1f8125dc15e8296121c18c9d8a3
Okawa <okawanori001@gmail.com>
2021-06-20 16:58:23 -0400

    Change signup modal

--- src/containers/modals/modal-sign-up/index.js
+++ src/containers/modals/modal-sign-up/index.js
@@ -3,7 +3,7 @@ import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
-import Button from '@components/buttons/button';
+import Button from '@components/Button';
 import Modal from '@components/modal';
 import Loader from '@components/loader';
 
@@ -11,19 +11,24 @@ import { closeSignupModal } from '@actions/modals.actions';
 import userActions from '@actions/user.actions';
 
 import { getAccount, getIsLoading } from '@selectors/user.selectors';
+import { getModalParams } from '@selectors/modal.selectors';
 
 import styles from './styles.module.scss';
 import { useSignMessage, useUserNameAvailable, useMyIP } from '@hooks/espa/user.hooks';
 
-const ModalSignUp = ({ className, title, textForIcon, icon }) => {
+const ModalSignUp = ({ className, title }) => {
   const dispatch = useDispatch();
+  const params = useSelector(getModalParams);
 
   const [userName, setUserName] = useState('');
-  const [email, setEmail] = useState('');
+  const [discordUserName, setDiscordUserName] = useState('');
+  const [email, setEmail] = useState(params?.email);
 
   const account = useSelector(getAccount);
   const isLoading = useSelector(getIsLoading);
   const signMsg = useSignMessage(account);
+
+  console.log('signMsg: ', signMsg)
   const isUserNameAvailable = useUserNameAvailable(userName);
   let myIP = useMyIP();
 
@@ -36,8 +41,6 @@ const ModalSignUp = ({ className, title, textForIcon, icon }) => {
     return regEx.test(String(username));
   };
 
-  const isUserNameValid = validateUserName(userName);
-
   const validateEmail = (email) => {
     const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regEx.test(String(email).toLowerCase());
@@ -45,10 +48,6 @@ const ModalSignUp = ({ className, title, textForIcon, icon }) => {
 
   const handleClick = () => {
     if (!signMsg) {
-      if (userName.length > 10) {
-        toast('Must not exceed 10 characters');
-        return;
-      }
       if (!validateUserName(userName)) {
         toast('User ID must contains letters and numbers only!');
         return;
@@ -58,7 +57,7 @@ const ModalSignUp = ({ className, title, textForIcon, icon }) => {
         return;
       }
     }
-    dispatch(userActions.tryToSignup(account, userName, email, signMsg, myIP));
+    dispatch(userActions.tryToSignup(account, userName, email, discordUserName, signMsg, myIP));
   };
 
   const userNameChanged = (username) => {
@@ -70,10 +69,12 @@ const ModalSignUp = ({ className, title, textForIcon, icon }) => {
       {createPortal(
         <Modal
           onClose={() => handleClose()}
-          title={title}
+          title={signMsg === null ? "CONNECTING WALLET"  : (signMsg === '' ? 'CREATE ACCOUNT' : 'SIGN IN HERE!') }
           className={(className, styles.modalWrapper)}
         >
-          <span>{`CURRENT ETH ADDRESS: ${account ? account : 'WALLET NOT CONNECTED'}`}</span>
+          <div className={styles.currentAddress}>
+            {`CURRENT ETH ADDRESS: ${account ? account : 'WALLET NOT CONNECTED'}`}
+          </div>
           {signMsg === null ? (
             <Loader size="large" className={styles.loader} />
           ) : (
@@ -85,22 +86,33 @@ const ModalSignUp = ({ className, title, textForIcon, icon }) => {
                   <>
                     <div className={styles.inputItem}>
                       <div className={styles.userIdRow}>
-                        <label>USER ID</label>
+                        <label>META ID</label>
                         <span className={styles.questionMark}>?</span>
                         <span className={styles.hint}>
                           Username must not exceed 10 characters. No special characters allowed,
                           only numbers and letters.
                         </span>
                       </div>
-                      <input value={userName} onChange={(e) => userNameChanged(e.target.value)} />
+                      <input value={userName} onChange={(e) => userNameChanged(e.target.value)} className="font-poppins text-base" />
                       {!isUserNameAvailable && (
                         <p>That User ID is already taken. Please choose another one</p>
                       )}
-                      {!isUserNameValid && <p>No special characters. Only letters and numbers.</p>}
                     </div>
+                    {/* <div className={styles.inputItem}>
+                      <label>DISCORD USERNAME</label>
+                      <input
+                        value={discordUserName}
+                        onChange={(e) => setDiscordUserName(e.target.value)}
+                      />
+                    </div> */}
                     <div className={styles.inputItem}>
                       <label>EMAIL</label>
-                      <input value={email} onChange={(e) => setEmail(e.target.value)} />
+                      <input
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        disabled={params?.email}
+                        className="font-poppins text-base"
+                      />
                     </div>
                   </>
                 ))}
@@ -127,15 +139,11 @@ const ModalSignUp = ({ className, title, textForIcon, icon }) => {
 ModalSignUp.propTypes = {
   className: PropTypes.string,
   title: PropTypes.string,
-  textForIcon: PropTypes.string,
-  icon: PropTypes.string,
 };
 
 ModalSignUp.defaultProps = {
   className: '',
-  title: 'CREATE AN ACCOUNT',
-  textForIcon: 'Metamask',
-  icon: './images/icons/metamask.svg',
+  title: 'CONNECTING WALLET',
 };
 
 export default ModalSignUp;
