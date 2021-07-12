a03bd05b704e273187af776631c1423d50cbe7d4
wj-code-mania <wonyy1988@outlook.com>
2021-07-12 09:14:07 -0700

    refactor: remove unnecessary imports, to use standard styles

--- src/pages/opensourcelibraries/index.js
+++ src/pages/opensourcelibraries/index.js
@@ -1,128 +1,130 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import CircleMenu from '../../components/circle-menu';
-import { Grid } from '@material-ui/core';
-import Icon from '@material-ui/core/Icon';
-import Popper from '@material-ui/core/Popper';
-import InputBase from '@material-ui/core/InputBase';
-import Fade from '@material-ui/core/Fade';
-
-import { getAllDesignerCIDs } from '@selectors/designer.selectors';
-import APIService from '@services/api/api.service';
+import React, { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { Grid } from '@material-ui/core'
+import CircleMenu from '@components/circle-menu'
+import { getAllDesignerCIDs } from '@selectors/designer.selectors'
+import APIService from '@services/api/api.service'
 
 function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
+  const {
+    innerWidth: width,
+    innerHeight: height
+  } = window
+
   return {
     width,
-    height,
-  };
+    height
+  }
 }
 
 function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions())
 
   useEffect(() => {
     function handleResize() {
-      setWindowDimensions(getWindowDimensions());
+      setWindowDimensions(getWindowDimensions())
     }
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
 
-  return windowDimensions;
+  return windowDimensions
 }
 
 function Libraries(props) {
-  const [open, setOpen] = useState(true);
-  const handleClose = () => setOpen(false);
+  const [open, setOpen] = useState(true)
+  const handleClose = () => setOpen(false)
 
-  const [items, setItems] = useState({});
+  const [items, setItems] = useState({})
 
-  const screenWidth = useWindowDimensions().width;
-  const [isMobile, setIsMobile] = useState(false);
+  const screenWidth = useWindowDimensions().width
+  const [isMobile, setIsMobile] = useState(false)
 
   useEffect(() => {
-    console.log('screen Width = >', screenWidth);
-    screenWidth > 796 ? setIsMobile(false) : setIsMobile(true);
-  }, [screenWidth]);
+    console.log('screen Width = >', screenWidth)
+    screenWidth > 796 ? setIsMobile(false) : setIsMobile(true)
+  }, [screenWidth])
 
-  const designerCIDs = useSelector(getAllDesignerCIDs());
+  const designerCIDs = useSelector(getAllDesignerCIDs())
 
   async function getData() {
-    const idLabel = 'Designer ID';
-    let ids = [];
+    const idLabel = 'Designer ID'
+    let ids = []
+
     for (const cid of designerCIDs) {
-      const res = await fetch(`https://digitalax.mypinata.cloud/ipfs/${cid}`);
-      const rdata = await res.json();
-      ids.push(rdata[idLabel]);
+      const res = await fetch(`https://digitalax.mypinata.cloud/ipfs/${cid}`)
+      const rdata = await res.json()
+      ids.push(rdata[idLabel])
     }
-    const result = await APIService.getMaterialVS();
-    const { digitalaxMaterialV2S } = result;
+
+    const result = await APIService.getMaterialVS()
+    const { digitalaxMaterialV2S } = result
+
     if (digitalaxMaterialV2S) {
-      let data = {};
+      let data = {}
       for (const item of digitalaxMaterialV2S) {
-        const res = await fetch(item.tokenUri);
-        const rdata = await res.json();
+        const res = await fetch(item.tokenUri)
+        const rdata = await res.json()
         if (rdata['image_url'] && rdata[idLabel] && ids.includes(rdata[idLabel])) {
-          const designerId = rdata[idLabel];
+          const designerId = rdata[idLabel]
           if (!data[designerId]) {
-            data[designerId] = [];
+            data[designerId] = []
           }
           data[designerId].push({
             name:
               rdata['attributes'] && rdata['attributes'].length > 0 && rdata['attributes'][0].value,
             image: rdata['image_url'],
             description: rdata['description'],
-          });
+          })
         }
       }
-      setItems(data);
+      setItems(data)
     }
   }
 
   useEffect(() => {
-    getData();
-  }, []);
+    getData()
+  }, [])
 
   return (
     <div style={{ marginBottom: 30 }}>
       {!isMobile ? (
         <>
-          <div className="flex my-5 items-center">
-            <img src="/images/on-chain.png" className="onchainimg" alt="" />
-            <img src="/images/opensource.png" className="opensourceimg" alt="" />
+          <div className='flex my-5 items-center'>
+            <img src='/images/on-chain.png' className='onchainimg' alt='' />
+            <img src='/images/opensource.png' className='opensourceimg' alt='' />
           </div>
-          <div className="flex my-5 items-center">
-            <img src="/images/llss.png" className="llssimg" alt="" />
-            <img src="/images/nft.png" className="nftimg" alt="" />
-            <img src="/images/libraries.png" className="librariesimg" alt="" />
+          <div className='flex my-5 items-center'>
+            <img src='/images/llss.png' className='llssimg' alt='' />
+            <img src='/images/nft.png' className='nftimg' alt='' />
+            <img src='/images/libraries.png' className='librariesimg' alt='' />
           </div>
-          <div className="flex justify-center my-10 w-full">
-            <img src="/images/texture.png" alt="" />
-            <img src="/images/material.png" className="mx-10 materialimg" alt="" />
-            <img src="/images/pattern.png" alt="" />
+          <div className='flex justify-center my-10 w-full'>
+            <img src='/images/texture.png' alt='' />
+            <img src='/images/material.png' className='mx-10 materialimg' alt='' />
+            <img src='/images/pattern.png' alt='' />
           </div>
-          <div className="flex my-16">
-            <img src="/images/designer_contribution.png" className="mx-auto" alt="" />
+          <div className='flex my-16'>
+            <img src='/images/designer_contribution.png' className='mx-auto' alt='' />
           </div>
         </>
       ) : (
         <>
-          <div className="flex flex-col my-5 items-center opensourceheader">
-            <img src="/images/on-chain.png" className="onchainimg" alt="" />
-            <img src="/images/llss.png" className="llssimg" alt="" />
-            <img src="/images/nft.png" className="nftimg" alt="" />
-            <img src="/images/opensource.png" className="opensourceimg" alt="" />
-            <img src="/images/libraries.png" className="librariesimg" alt="" />
-            <img src="/images/texture.png" alt="" className="flex" />
-            <img src="/images/material.png" className="materialimg" alt="" />
-            <img src="/images/pattern.png" alt="" className="flex" />
-            <img src="/images/designer.png" alt="" className="flex" />
+          <div className='flex flex-col my-5 items-center opensourceheader'>
+            <img src='/images/on-chain.png' className='onchainimg' alt='' />
+            <img src='/images/llss.png' className='llssimg' alt='' />
+            <img src='/images/nft.png' className='nftimg' alt='' />
+            <img src='/images/opensource.png' className='opensourceimg' alt='' />
+            <img src='/images/libraries.png' className='librariesimg' alt='' />
+            <img src='/images/texture.png' alt='' className='flex' />
+            <img src='/images/material.png' className='materialimg' alt='' />
+            <img src='/images/pattern.png' alt='' className='flex' />
+            <img src='/images/designer.png' alt='' className='flex' />
             <img
-              src="/images/contribution.png"
-              alt=""
-              className="flex"
+              src='/images/contribution.png'
+              alt=''
+              className='flex'
               style={{ marginTop: '-21px' }}
             />
           </div>
@@ -132,7 +134,7 @@ function Libraries(props) {
         <Grid container item xs={12}>
           {Object.keys(items).map((key, index) => (
             // eslint-disable-next-line react/no-array-index-key
-            <Grid container item xs={4} justify="center" key={`circle-${index}`}>
+            <Grid container item xs={4} justify='center' key={`circle-${index}`}>
               <CircleMenu
                 items={items[key]}
                 keyName={key}
@@ -143,12 +145,12 @@ function Libraries(props) {
         </Grid>
       ) : (
         <div style={{ width: 375, maxWidth: 375}}>
-          <div className="opensourcecontent">
+          <div className='opensourcecontent'>
             {/* <Grid container item xs={12}> */}
             {Object.keys(items).map((key, index) => (
               // eslint-disable-next-line react/no-array-index-key
-              <Grid container item xs={12} justify="center" key={`circle-${index}`}>
-                <CircleMenu items={items[key]} keyName={key} direction="Right" />
+              <Grid container item xs={12} justify='center' key={`circle-${index}`}>
+                <CircleMenu items={items[key]} keyName={key} direction='Right' />
               </Grid>
             ))}
             {/* </Grid> */}
@@ -156,7 +158,7 @@ function Libraries(props) {
         </div>
       )}
     </div>
-  );
+  )
 }
 
-export default Libraries;
+export default Libraries
