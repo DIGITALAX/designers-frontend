a7da977428d0bd4edbadbb8ac1a65361efa0d776
wj-code-mania <wonyy1988@outlook.com>
2021-08-24 09:43:57 -0700

    feat: show materials on designer profile page

--- src/pages/designer/[id]/index.js
+++ src/pages/designer/[id]/index.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { useSelector } from 'react-redux'
-import kebabCase from 'lodash.kebabcase'
 
 import { getDesignerCIDById } from '@selectors/designer.selectors'
+
+import APIService from '@services/api/api.service'
+import api from '@services/api/espa/api.service'
+
 import styles from './styles.module.scss'
 
 const DesignerPage = () => {
@@ -11,11 +14,100 @@ const DesignerPage = () => {
   const { id } = router.query
   const [data, setData] = useState(null)
   const designerCID = useSelector(getDesignerCIDById(id))
+  const [materialList, setMaterialList] = useState([])
+
+  async function getMaterials(designerInfo) {
+    const thumbnails = await api.getAllThumbnails()
+    
+    const thumbnailObj = {}
+    const blockedList = []
+    for (const thumbnail in thumbnails.data) {
+      const thumbItem = thumbnails.data[thumbnail]
+      thumbnailObj[thumbItem.image_url] = thumbItem.thumbnail_url
+      if (thumbItem.blocked) {
+        blockedList.push(thumbItem.image_url)
+      }
+    }
+
+    console.log('thumbnailObj: ', thumbnailObj)
+
+    // setThumbnailList(thumbnailObj)
+
+    const idLabel = 'Designer ID'
+
+    const result = await APIService.getMaterialVS()
+    const { digitalaxMaterialV2S } = result
+    const materials = []
+    // console.log('digitalaxMaterialV2S: ', digitalaxMaterialV2S)
+    let noThumbnailData = []
+    console.log('designer id: ', designerInfo['Designer ID'])
+    if (digitalaxMaterialV2S) {
+      for (const item of digitalaxMaterialV2S) {
+        // console.log('--- item: ', item)
+        const res = await fetch(item.tokenUri)
+        // console.log('--- item res: ', res)
+        const rdata = await res.json()
+        // console.log('--- item rdata: ', rdata)
+        if (!rdata['image_url'] || !rdata[idLabel]) continue
+        if (designerInfo['Designer ID'].toLowerCase() !== rdata[idLabel].toLowerCase()) continue
+        let designerId = rdata[idLabel]
+        if (!designerId || designerId === undefined || designerId === '') continue
+
+        if (blockedList.findIndex(item => item === rdata['image_url']) < 0) {
+          if (designerInfo['newDesignerID'] && designerInfo['newDesignerID'] !== undefined) {
+            designerId = designerInfo['newDesignerID']
+          }
+
+          // console.log('--rdata: ', rdata)
+
+          materials.push({
+            ...item,
+            name:
+              rdata['attributes'] && rdata['attributes'].length > 0 && rdata['attributes'][0].value,
+            image: rdata['image_url'],
+            thumbnail: thumbnailObj ? thumbnailObj[rdata['image_url']] : null,
+            description: rdata['description']
+          })
+
+          setMaterialList([...materials])
+
+          // if (!data[designerId]) {
+          //   data[designerId] = []
+          // }
+          // if (data[designerId].findIndex(item => item.image === rdata['image_url']) >= 0) {
+          //   continue
+          // }
+
+          // if (!thumbnailObj[rdata['image_url']]) {
+          //   noThumbnailData.push({
+          //     designerId,
+          //     image_url: rdata['image_url'],
+          //     thumbnail: ''
+          //   })
+          // }
+
+          // data[designerId].push({
+          //   ...item,
+          //   name:
+          //     rdata['attributes'] && rdata['attributes'].length > 0 && rdata['attributes'][0].value,
+          //   image: rdata['image_url'],
+          //   thumbnail: thumbnailObj ? thumbnailObj[rdata['image_url']] : null,
+          //   description: rdata['description'],
+            
+          // })
+          // setItems({...data})
+        }
+      }
+    }
+  }
 
   useEffect(() => {
     fetch(`https://digitalax.mypinata.cloud/ipfs/${designerCID}`)
       .then((response) => response.json())
-      .then((jsonData) => setData(jsonData))
+      .then(jsonData => {
+        setData(jsonData)
+        getMaterials(jsonData)
+      })
       .catch((error) => {
         console.error(error)
       })
@@ -25,7 +117,7 @@ const DesignerPage = () => {
     return null;
   }
 
-  console.log('data: ', data)
+  console.log('materialList: ', materialList)
 
   return (
     <div className={styles.wrapper}>
@@ -45,14 +137,46 @@ const DesignerPage = () => {
         <div className={styles.designerDescription}>
           { data['description'] }
         </div>
-
-        <div className={styles.patternWrapper}>
-          <img className={styles.pattern1} src='/images/designer-page/pattern-selection.png' />
-          <img className={styles.pattern2} src='/images/designer-page/pattern-selection.png' />
-          <img className={styles.pattern3} src='/images/designer-page/pattern-selection.png' />
-          <img className={styles.pattern4} src='/images/designer-page/pattern-selection.png' />
-          <img className={styles.pattern5} src='/images/designer-page/pattern-selection.png' />
+        <div className={styles.patternSection}>
+          <div className={styles.patternWrapper3}>
+            {
+              materialList.slice(0, 5).map((item, index) => {
+                return (
+                  <img 
+                    className={styles.patternCircle} src={item.thumbnail ? item.thumbnail : item.image}
+                    key={index}
+                    style={{
+                      marginLeft: `${index % 2 * 30}%`
+                    }}
+                  />
+                )
+              })
+            }
+          </div>
+          <div className={styles.patternWrapper4}>
+            {
+              materialList.slice(6, materialList.length).map((item, index) => {
+                return (
+                  <img 
+                    className={styles.patternCircle} src={item.thumbnail ? item.thumbnail : item.image}
+                    key={index}
+                    style={{
+                      marginTop: `${index % 3 === 1 ? -10 : 0}vw`
+                    }}
+                  />
+                )
+              })
+            }
+          </div>
+          <div className={styles.patternWrapper}>
+            <img className={styles.pattern1} src='/images/designer-page/pattern-selection.png' />
+            <img className={styles.pattern2} src='/images/designer-page/pattern-selection.png' />
+            <img className={styles.pattern3} src='/images/designer-page/pattern-selection.png' />
+            <img className={styles.pattern4} src='/images/designer-page/pattern-selection.png' />
+            <img className={styles.pattern5} src='/images/designer-page/pattern-selection.png' />
+          </div>
         </div>
+
 
         <div className={styles.patternWrapper2}>
           <img className={styles.patternSample1} src='/images/designer-page/pattern_sample1.png' />
