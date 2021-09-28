d867b29604644416b48a60071b5a42b3865a65af
jupiteruwdev <harutoinoue8@gmail.com>
2021-09-28 11:07:55 -0400

    get dressed page updated.

--- src/actions/dressed.actions.js
+++ src/actions/dressed.actions.js
@@ -1,24 +1,25 @@
 import { utils as ethersUtils } from 'ethers';
-import { getMonaTokenContract } from "@services/contract.service";
-import { getMonaContractAddressByChainId } from "@services/network.service";
+import { getMonaTokenContract } from '@services/contract.service';
+import { getMonaContractAddressByChainId } from '@services/network.service';
 import { convertToWei } from '@helpers/price.helpers';
 import api from '@services/api/espa/api.service';
 
 const address = '0x3Cc6Bc27c36d23Ed95289BAF5b0bcB8538D1467c';
+const ethAddress = '0x981b2cfe8c29757c1d5e5862b6f268b9792ffa32';
 
 class DressedActions {
   async isApproved(wallet, chainId) {
     const monaContractAddress = getMonaContractAddressByChainId(chainId);
     const contract = await getMonaTokenContract(monaContractAddress);
     try {
-      const allowance = await contract.methods.allowance(wallet, address).call({from: wallet});
+      const allowance = await contract.methods.allowance(wallet, address).call({ from: wallet });
       const jsAllowedValue = parseFloat(ethersUtils.formatEther(allowance));
       if (jsAllowedValue < 10000000000) {
         return false;
       }
       return true;
-    } catch (e){
-      console.log({e});
+    } catch (e) {
+      console.log({ e });
       throw e;
     }
   }
@@ -27,24 +28,31 @@ class DressedActions {
     const monaContractAddress = getMonaContractAddressByChainId(chainId);
     const contract = await getMonaTokenContract(monaContractAddress);
     try {
-      await contract.methods.approve(address, convertToWei(20000000000))
-        .send({from: wallet});
-    } catch (e){
-      console.log({e});
+      await contract.methods.approve(address, convertToWei(20000000000)).send({ from: wallet });
+    } catch (e) {
+      console.log({ e });
       throw e;
     }
   }
+
   async sendMona(wallet, chainId, value) {
     const monaContractAddress = getMonaContractAddressByChainId(chainId);
     const contract = await getMonaTokenContract(monaContractAddress);
     try {
       const res = await contract.methods
         .transfer(address, convertToWei(value))
-        .send({from: wallet});
-    } catch (e){
-      console.log({e});
+        .send({ from: wallet });
+    } catch (e) {
+      console.log({ e });
       throw e;
     }
+  }
+
+  async sendEth(wallet, value) {
+    const web3 = window.web3;
+
+    const amountToSend = convertToWei(value); // Convert to wei value
+    var send = web3.eth.sendTransaction({ from: wallet, to: ethAddress, value: amountToSend });
   }
 
   async uploadImage(file) {
@@ -67,6 +75,6 @@ class DressedActions {
       throw e;
     }
   }
-};
+}
 
 export default new DressedActions();
