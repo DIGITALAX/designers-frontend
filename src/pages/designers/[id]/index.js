863a3f9b19651f8a500df4cf5c60fae150a9ad07
jupiteruwdev <harutoinoue8@gmail.com>
2021-10-04 12:25:17 -0400

    designers profile updated.

--- src/pages/designers/[id]/index.js
+++ src/pages/designers/[id]/index.js
@@ -1,45 +1,46 @@
-import React, { useState, useEffect } from 'react'
-import { useRouter } from 'next/router'
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-import { EXCLUSIVE_RARITY, COMMON_RARITY, SEMI_RARE_RARITY } from '@constants/global.constants'
+import { EXCLUSIVE_RARITY, COMMON_RARITY, SEMI_RARE_RARITY } from '@constants/global.constants';
 
-import APIService from '@services/api/api.service'
-import api from '@services/api/espa/api.service'
+import APIService from '@services/api/api.service';
+import api from '@services/api/espa/api.service';
 
-import DesignerProfileTopPart from '@components/DesignerProfile/TopPart'
-import DesignerProfileBottomPart from '@components/DesignerProfile/BottomPart'
+import DesignerProfileTopPart from '@components/DesignerProfile/TopPart';
+import DesignerProfileBottomPart from '@components/DesignerProfile/BottomPart';
 
-import styles from './styles.module.scss'
+import styles from './styles.module.scss';
 
-const RARITIES = [
-  COMMON_RARITY, EXCLUSIVE_RARITY, SEMI_RARE_RARITY
-]
+const RARITIES = [COMMON_RARITY, EXCLUSIVE_RARITY, SEMI_RARE_RARITY];
 
-const getRarityNumber = rarity => RARITIES.findIndex(item => item == rarity)
+const getRarityNumber = (rarity) => RARITIES.findIndex((item) => item == rarity);
 
 const DesignerPage = () => {
-  const router = useRouter()
-  const { id } = router.query
-  const [designerInfo, setDesignerInfo] = useState(null)
-  const [materialList, setMaterialList] = useState([])
-  const [marketplaceItems, setMarketplaceItems] = useState([])
+  const router = useRouter();
+  const { id } = router.query;
+  const [designerInfo, setDesignerInfo] = useState(null);
+  const [materialList, setMaterialList] = useState([]);
+  const [marketplaceItems, setMarketplaceItems] = useState([]);
 
   async function loadData() {
-    const designers = await api.getAllDesigners() || []
-    const thumbnails = await api.getAllThumbnails()
+    const designers = (await api.getAllDesigners()) || [];
+    const thumbnails = await api.getAllThumbnails();
 
-    const designer = designers.find(item => item.designerId.toLowerCase() === id.toLowerCase()
-    || (item.newDesignerID && item.newDesignerID.toLowerCase() === id.toLowerCase()))
+    const designer = designers.find(
+      (item) =>
+        item.designerId.toLowerCase() === id.toLowerCase() ||
+        (item.newDesignerID && item.newDesignerID.toLowerCase() === id.toLowerCase())
+    );
 
-    setDesignerInfo(designer)
-      
-    const thumbnailObj = {}
-    const blockedList = []
+    setDesignerInfo(designer);
+
+    const thumbnailObj = {};
+    const blockedList = [];
     for (const thumbnail in thumbnails.data) {
-      const thumbItem = thumbnails.data[thumbnail]
-      thumbnailObj[thumbItem.image_url] = thumbItem.thumbnail_url
+      const thumbItem = thumbnails.data[thumbnail];
+      thumbnailObj[thumbItem.image_url] = thumbItem.thumbnail_url;
       if (thumbItem.blocked) {
-        blockedList.push(thumbItem.image_url)
+        blockedList.push(thumbItem.image_url);
       }
     }
 
@@ -47,105 +48,127 @@ const DesignerPage = () => {
 
     // setThumbnailList(thumbnailObj)
 
-    const idLabel = 'Designer ID'
+    const idLabel = 'Designer ID';
 
-    const result = await APIService.getMaterialVS()
-    const { digitalaxMaterialV2S } = result
+    const result = await APIService.getMaterialVS();
+    const { digitalaxMaterialV2S } = result;
 
-    const { digitalaxCollectionGroups } = await APIService.getCollectionGroups()
+    const { digitalaxCollectionGroups } = await APIService.getCollectionGroups();
     // console.log('digitalaxCollectionGroups: ', digitalaxCollectionGroups)
-    const auctionItems = []
-    digitalaxCollectionGroups.forEach(group => {
-      auctionItems.push(
-        ...group.auctions.filter(
-          auctionItem => {
-            return auctionItem.designer.name.toLowerCase() === designer['designerId'].toLowerCase()
-          }
-        ).map(item => {
-          // console.log('item: ', item)
-          return {
-            ...item.garment,
-            isAuction: 1
-          }
-        })
-      )
-
-      group.collections.filter(
-        collectionItem => {
-          return collectionItem.designer.name.toLowerCase() === designer['designerId'].toLowerCase()
-        }
-      ).forEach(item => {
+    const auctionItems = [];
+    digitalaxCollectionGroups.forEach((group) => {
+      if (!(groupd.auctions.length === 1 && group.auctions[0].id === '0')) {
         auctionItems.push(
-          ...item.garments.map(garment => { return {...garment, rarity: getRarityNumber(item.rarity), isAuction: 0, id: item.id}})
-        )
-      })
-    })
+          ...group.auctions
+            .filter((auctionItem) => {
+              return (
+                auctionItem.designer.name.toLowerCase() === designer['designerId'].toLowerCase()
+              );
+            })
+            .map((item) => {
+              // console.log('item: ', item)
+              return {
+                ...item.garment,
+                isAuction: 1,
+              };
+            })
+        );
+      }
 
-    setMarketplaceItems(auctionItems)
+      if (!(group.collections.length === 1 && gropud.collections[0].id === '0')) {
+        group.collections
+          .filter((collectionItem) => {
+            return (
+              collectionItem.designer.name.toLowerCase() === designer['designerId'].toLowerCase()
+            );
+          })
+          .forEach((item) => {
+            auctionItems.push(
+              ...item.garments.map((garment) => {
+                return {
+                  ...garment,
+                  rarity: getRarityNumber(item.rarity),
+                  isAuction: 0,
+                  id: item.id,
+                };
+              })
+            );
+          });
+      }
+    });
+
+    setMarketplaceItems(auctionItems);
     // console.log('auctionItems: ', auctionItems)
 
-    const materials = []
+    const materials = [];
     // console.log('digitalaxMaterialV2S: ', digitalaxMaterialV2S)
-    let noThumbnailData = []
+    let noThumbnailData = [];
     // console.log('designer id: ', designerInfo['Designer ID'])
     if (digitalaxMaterialV2S) {
       for (const item of digitalaxMaterialV2S) {
-        if (item.attributes.length <= 0) continue
+        if (item.attributes.length <= 0) continue;
         try {
-          const res = await fetch(item.tokenUri)
+          const res = await fetch(item.tokenUri);
           // console.log('--- item res: ', res)
-          const rdata = await res.json()
+          const rdata = await res.json();
           // console.log('--- item rdata: ', rdata)
-          if (!rdata['image_url'] || !rdata[idLabel]) continue
-          if (designer['designerId'].toLowerCase() !== rdata[idLabel].toLowerCase() &&
-                (
-                  !designer['newDesignerID'] || 
-                  designer['newDesignerID'] === '' || 
-                  designer['newDesignerID'].toLowerCase() !== rdata[idLabel].toLowerCase()
-                )
-              ) continue
-          let designerId = rdata[idLabel]
-          if (!designerId || designerId === undefined || designerId === '') continue
+          if (!rdata['image_url'] || !rdata[idLabel]) continue;
+          if (
+            designer['designerId'].toLowerCase() !== rdata[idLabel].toLowerCase() &&
+            (!designer['newDesignerID'] ||
+              designer['newDesignerID'] === '' ||
+              designer['newDesignerID'].toLowerCase() !== rdata[idLabel].toLowerCase())
+          )
+            continue;
+          let designerId = rdata[idLabel];
+          if (!designerId || designerId === undefined || designerId === '') continue;
 
-          if (blockedList.findIndex(item => item === rdata['image_url']) < 0) {
+          if (blockedList.findIndex((item) => item === rdata['image_url']) < 0) {
             if (designer['newDesignerID'] && designer['newDesignerID'] !== undefined) {
-              designerId = designer['newDesignerID']
+              designerId = designer['newDesignerID'];
             }
 
             // console.log('--rdata: ', rdata)
-            if (materials.findIndex(item => item.image === rdata['image_url']) >= 0) continue
+            if (materials.findIndex((item) => item.image === rdata['image_url']) >= 0) continue;
             materials.push({
               ...item,
               name:
-                rdata['attributes'] && rdata['attributes'].length > 0 && rdata['attributes'][0].value,
+                rdata['attributes'] &&
+                rdata['attributes'].length > 0 &&
+                rdata['attributes'][0].value,
               image: rdata['image_url'],
               thumbnail: thumbnailObj ? thumbnailObj[rdata['image_url']] : null,
-              description: rdata['description']
-            })
+              description: rdata['description'],
+            });
 
-            setMaterialList([...materials])
+            setMaterialList([...materials]);
           }
         } catch (exception) {
-          console.log('exception: ', exception)
+          console.log('exception: ', exception);
         }
       }
     }
   }
 
   useEffect(() => {
-    loadData()
-  }, [])
+    loadData();
+  }, []);
 
   if (!designerInfo) {
     return (
       <div className={styles.beforeLoading}>
-        <div className={styles.ldsEllipsis}><div></div><div></div><div></div><div></div></div>
+        <div className={styles.ldsEllipsis}>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
-    )
+    );
   }
 
   // console.log('designerInfo: ', designerInfo)
-  console.log('materialList: ', materialList)
+  console.log('materialList: ', materialList);
 
   return (
     <div className={styles.wrapper}>
@@ -155,12 +178,9 @@ const DesignerPage = () => {
         materialList={materialList}
         marketplaceItems={marketplaceItems}
       />
-      <DesignerProfileBottomPart 
-        designerInfo={designerInfo}
-        isEditable={false}
-      />
+      <DesignerProfileBottomPart designerInfo={designerInfo} isEditable={false} />
     </div>
-  )
-}
+  );
+};
 
-export default DesignerPage
+export default DesignerPage;
